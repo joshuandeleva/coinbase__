4495ed7272cdf5cda81971698d31635b5b519547
joshuandeleva <joshuandeleva90@gmail.com>
2021-07-06 11:28:41 +0300

    constant routes

---
+++ src/constants/routes.js
@@ -0,0 +1,7 @@
+export const HOME ='/'
+export const LOGIN='/login'
+export const REGISTER='/register'
+export const ABOUTUS='/about-us'
+export const SELL='/sell-bitcoin'
+export const BUY = "/buy-bitcoin";
+export const CONTACTUS = "/contact-us";
