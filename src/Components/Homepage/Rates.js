6e33d74ae1be54aa214f162771d274dc543583f2
joshuandeleva <joshuandeleva90@gmail.com>
2021-07-09 15:46:12 +0300

    display data from api

--- src/Components/Homepage/Rates.js
+++ src/Components/Homepage/Rates.js
@@ -1,23 +1,41 @@
 import React, { useEffect, useState } from "react";
 import "./Rates.css";
 import axios from "axios";
-import { Container, Grid } from "@material-ui/core";
+import { Container } from "@material-ui/core";
 function Rates() {
-	const [apiData, setApiData] = useState("");
-	const API_KEY = "fc19a462c4720c420afbc478910aafffb91d11af";
+	const [apiData, setApiData] = useState([]);
+	//const API_KEY = "fc19a462c4720c420afbc478910aafffb91d11af";
 	useEffect(() => {
 		const fetchData = async () => {
 			const resultsFromApi = await axios.get(
-				"https://api.coingecko.com/api/v3/coins/markets?vs_currency=EUR&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+				"https://api.coingecko.com/api/v3/coins/markets?vs_currency=EUR&order=market_cap_desc&per_page=10&page=1&sparkline=false"
 			);
-			console.log(resultsFromApi);
-			setApiData(resultsFromApi.apiData);
+			console.log(resultsFromApi.data);
+			setApiData(resultsFromApi.data);
 		};
 		fetchData();
-	});
+	}, []);
 	return (
-		<div className="Rates-card">
-			<Container></Container>
+		<div className="Rates-cardMain">
+			<Container>
+				{apiData &&
+					apiData.map((rateItem, index) => {
+						return (
+							<div className="rateCard" key={index}>
+								<div className="rateCard__Left">
+									<img src={rateItem.image} alt="" />
+								</div>
+								<div className="rateCard__Right">
+									<h1>{rateItem.symbol.toUpperCase()}</h1>
+
+									<span>
+										${rateItem.current_price.toFixed(1)}
+									</span>
+								</div>
+							</div>
+						);
+					})}
+			</Container>
 		</div>
 	);
 }
